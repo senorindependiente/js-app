11e483a0ed78a492c24dd74eeac4b96903ad929a
senorindependiente <pingelchristian@hotmail.com>
2022-01-17 16:29:59 +0100

    Added objects inside the array

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,20 @@
-let pokemonList = []
+let pokemonList = [
+  {
+    name: "Liepard",
+    height: 1.1,
+    types: [dark, fairy, fighting, ghost, psychic, bug]
+  },
+  { name: "Pikachu", height: 0.4, types: [electric, steel, flying] },
+  { name: "Persian", height: 1, types: [normal, fighting, ghost] },
+  {
+    name: "Chimchar",
+    height: 0.5,
+    types: [fire, ground, rock, water, steel, ice, fairy]
+  },
+  {
+    name: "Wailord",
+    height: 14.5,
+    types: [water, grass, rock, electric, steel, fire, ice]
+  },
+  { name: "Beartic", height: 2.6, types: [fighting, rock, steel, fire, ice] },
+];
