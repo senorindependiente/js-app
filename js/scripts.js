beeb66d6a96cafde1189193e952e9ade47118be4
senorindependiente <pingelchristian@hotmail.com>
2022-01-18 14:39:31 +0100

    Added strings for each types array value

--- js/scripts.js
+++ js/scripts.js
@@ -2,19 +2,19 @@ let pokemonList = [
   {
     name: "Liepard",
     height: 1.1,
-    types: [dark, fairy, fighting, ghost, psychic, bug]
+    types: ["dark", "fairy", "fighting", "ghost", "psychic", "bug"]
   },
-  { name: "Pikachu", height: 0.4, types: [electric, steel, flying] },
-  { name: "Persian", height: 1, types: [normal, fighting, ghost] },
+  { name: "Pikachu", height: 0.4, types: ["electric", "steel", "flying"] },
+  { name: "Persian", height: 1, types: ["normal", "fighting", "ghost"] },
   {
     name: "Chimchar",
     height: 0.5,
-    types: [fire, ground, rock, water, steel, ice, fairy]
+    types: ["fire", "ground", "rock", "water", "steel", "ice", "fairy"]
   },
   {
     name: "Wailord",
     height: 14.5,
-    types: [water, grass, rock, electric, steel, fire, ice]
+    types: ["water", "grass", "rock", "electric", "steel", "fire", "ice"]
   },
-  { name: "Beartic", height: 2.6, types: [fighting, rock, steel, fire, ice] },
+  { name: "Beartic", height: 2.6, types: ["fighting", "rock", "steel", "fire", "ice"] },
 ];
