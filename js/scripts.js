479f63c2d23eac5ba24669d31d569aa4ea316225
senorindependiente <pingelchristian@hotmail.com>
2022-01-18 14:57:51 +0100

    Added loop to show names and height tinside the browser
    Added loop to display the name and the height of each pokemon in the pokemon list in the browser with document.write()

--- js/scripts.js
+++ js/scripts.js
@@ -2,19 +2,27 @@ let pokemonList = [
   {
     name: "Liepard",
     height: 1.1,
-    types: ["dark", "fairy", "fighting", "ghost", "psychic", "bug"]
+    types: ["dark", "fairy", "fighting", "ghost", "psychic", "bug"],
   },
   { name: "Pikachu", height: 0.4, types: ["electric", "steel", "flying"] },
   { name: "Persian", height: 1, types: ["normal", "fighting", "ghost"] },
   {
     name: "Chimchar",
     height: 0.5,
-    types: ["fire", "ground", "rock", "water", "steel", "ice", "fairy"]
+    types: ["fire", "ground", "rock", "water", "steel", "ice", "fairy"],
   },
   {
     name: "Wailord",
     height: 14.5,
-    types: ["water", "grass", "rock", "electric", "steel", "fire", "ice"]
+    types: ["water", "grass", "rock", "electric", "steel", "fire", "ice"],
   },
-  { name: "Beartic", height: 2.6, types: ["fighting", "rock", "steel", "fire", "ice"] },
+  {
+    name: "Beartic",
+    height: 2.6,
+    types: ["fighting", "rock", "steel", "fire", "ice"],
+  },
 ];
+
+for (i = 0; i < pokemonList.length; i++) {
+  document.write(pokemonList[i].name + " (height: " + pokemonList[i].height + " )");
+}
