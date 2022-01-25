7a85a7698c8b1b20e547e4493c3f38a1e06d1e1c
senorindependiente <pingelchristian@hotmail.com>
2022-01-25 13:08:38 +0100

    Empty pokemonList array and added Pokemon API

--- js/scripts.js
+++ js/scripts.js
@@ -2,30 +2,9 @@
 
 // "pokemonList" is wrapped into an IIFE and created new variable "pokemonRepository"
 let pokemonRepository = (function () {
-  let pokemonList = [
-    {
-      name: "Liepard",
-      height: 1.1,
-      types: ["dark", "fairy", "fighting", "ghost", "psychic", "bug"],
-    },
-    { name: "Pikachu", height: 0.4, types: ["electric", "steel", "flying"] },
-    { name: "Persian", height: 1, types: ["normal", "fighting", "ghost"] },
-    {
-      name: "Chimchar",
-      height: 0.5,
-      types: ["fire", "ground", "rock", "water", "steel", "ice", "fairy"],
-    },
-    {
-      name: "Wailord",
-      height: 14.5,
-      types: ["water", "grass", "rock", "electric", "steel", "fire", "ice"],
-    },
-    {
-      name: "Beartic",
-      height: 2.6,
-      types: ["fighting", "rock", "steel", "fire", "ice"],
-    },
-  ];
+  let pokemonList = [];
+  //Pokemon API
+  let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
 
   //added function to return the "pokemonList"
   function getAll() {
