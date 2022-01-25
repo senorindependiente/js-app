e93825933686ed0fed9d3d5ce6d93d67a132f614
senorindependiente <pingelchristian@hotmail.com>
2022-01-25 15:10:20 +0100

    Replaced static list of pokemon with the complete list of pokemon from the pokemon API
    Added functions to load and display the pokemon list from the API on button click

--- js/scripts.js
+++ js/scripts.js
@@ -4,7 +4,7 @@
 let pokemonRepository = (function () {
   let pokemonList = [];
   //Pokemon API
-  let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
+  let apiUrl = "https://pokeapi.co/api/v2/pokemon/?limit=150";
 
   //added function to return the "pokemonList"
   function getAll() {
@@ -14,23 +14,22 @@ let pokemonRepository = (function () {
   //added function to add new pokemen to the "pokemonList"
   function add(pokemon) {
     //checks that only certain properties will be accepted when adding a new pokemon
-    if(
+    if (
       typeof pokemon === "object" &&
       "name" in pokemon &&
-      "height" in pokemon &&
-      "types" in pokemon
+      "detailsUrl" in pokemon
     ) {
       //if property has been validated, new pokemon will be added
-    pokemonList.push(pokemon);}
-    else {
-      // otherwise error message 
+      pokemonList.push(pokemon);
+    } else {
+      // otherwise error message
       console.log("error");
     }
   }
 
   //added function to create buttons which display each pokemon in HTML
   function addListItem(pokemon) {
-// selecting elements
+    // selecting elements
     let list = document.querySelector(".pokemon-list");
     let listItem = document.createElement("li");
     let button = document.createElement("button");
@@ -44,27 +43,89 @@ let pokemonRepository = (function () {
     list.appendChild(listItem);
 
     //adding eventhandler to the button that will show the logged pokemon  on click
-    button.addEventListener("click", () => {
-      showDetails(pokemon.name);
+    button.addEventListener("click", (event) => {
+      showDetails(pokemon);
     });
   }
 
   //adding function to log the name of the pokemon
-  function showDetails(pokemon) {
-    console.log(pokemon);
+  //function showDetails(pokemon) {
+  //console.log(pokemon);
+  //}
+
+  //adding promise function
+  function loadList() {
+    //using the fetch method to get pokemon API "apiUrl", the result will be the "response", which is a promise
+    return fetch(apiUrl)
+      .then(function (response) {
+        // the reponse is in a json file which will be converted from strings to objects
+        return response.json();
+      })
+      //we run a forEach loop on the json
+      .then(function (json) {
+        //"json" object is the file with all the pokemon . the "results" is a json key taken from the json file list,
+        //the forEach loop will iterate trough all the different pokemon in the json file.
+        json.results.forEach(function (item) {
+          //for each of these pokemon item a variable will be created "pokemon" and turned into an object with two key-value pairs
+          let pokemon = {
+            //"item." here is the parameter of function, "name" and "url" are keys taken from the json file list
+            name: item.name,
+            detailsUrl: item.url,
+          };
+          //calling add function to
+          add(pokemon);
+        });
+      })
+      .catch(function (e) {
+        console.error(e);
+      });
   }
 
-  
-  //IIFE will return an object with  methods "getAll" and "add" and "addListItem"
+  // use the "detailsUrl" property to load the detailed data for a given Pokémon.
+  //for this, you add a loadDetails() function, which takes a Pokémon item as an argument
+  function loadDetails(item) {
+    let url = item.detailsUrl;
+    return fetch(url)
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (details) {
+        // Now we add the details to the item
+        //"item." is from the loadDetails parameter, "imageUrl" is a variable that was just created, "details."
+        //is the parameter of this function, "sprites.height.types." are thaken from the second  json file list when you click on the "url" in the first json file list
+        item.imageUrl = details.sprites.front_default;
+        item.height = details.height;
+        item.types = details.types;
+      })
+      .catch(function (e) {
+        console.error(e);
+      });
+  }
+
+  //the showDetails() function is executed when a user clicks on a Pokémon and you get the Pokémon’s details from the server.
+  //showDetails function will execute
+  function showDetails(item) {
+    //the loadDetails function with pokemon as parameter and adds a function as a promise and then to return a console log
+    loadDetails(item).then(function () {
+      console.log(item);
+    });
+  }
+
+  //IIFE will return an object with  methods to have access to these previously created functions
   return {
     getAll: getAll,
     add: add,
-    addListItem: addListItem
+    addListItem: addListItem,
+    loadList: loadList,
+    loadDetails: loadDetails,
   };
 })();
 
-// IIFE returned function to retrieve "pokemonList"
-//forEach method iterate trough each pokemon to display button with pokemon name in the browser
-pokemonRepository.getAll().forEach((pokemon) => {
-  pokemonRepository.addListItem(pokemon);
+//this will call the api to display
+pokemonRepository.loadList().then(function () {
+  // IIFE returned function to retrieve "pokemonList"
+  //forEach method iterate trough each pokemon to display button with pokemon name in the browser
+  pokemonRepository.getAll().forEach((pokemon) => {
+    pokemonRepository.addListItem(pokemon);
+  });
 });
