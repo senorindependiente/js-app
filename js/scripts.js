405a4fcab8ac55d60d2bf2b3816204017ab15a36
senorindependiente <pingelchristian@hotmail.com>
2022-02-02 12:59:09 +0100

    Added search functionality
    Added  search bar to search for a pokemon by name

--- js/scripts.js
+++ js/scripts.js
@@ -157,3 +157,22 @@ pokemonRepository.loadList().then(function () {
     pokemonRepository.addListItem(pokemon);
   });
 });
+
+
+//Added name filter function
+function myFunction() {
+  var input, filter, ul, li, btn, i, txtValue;
+  input = document.getElementById("myInput");
+  filter = input.value.toUpperCase();
+  ul = document.querySelector(".pokemon-list");
+  li = ul.getElementsByTagName("li");
+  for (i = 0; i < li.length; i++) {
+      btn = li[i].getElementsByTagName("button")[0];
+      txtValue = btn.textContent || btn.innerText;
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+          li[i].style.display = "";
+      } else {
+          li[i].style.display = "none";
+      }
+  }
+}
